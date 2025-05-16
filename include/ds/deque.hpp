9d194f2720e87f30b3015b3957dd757a77cf005f
carlos-paz12 <capz.devel@gmail.com>
2025-05-16 08:37:57 -0300

    creates initial library directory structure

---
+++ include/ds/deque.hpp
@@ -0,0 +1,11 @@
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
+namespace ds
+{
+ /// @todo
+}
+
+#include "deque.tpp" // Implementação da classe `Deque`.
+
+#endif //!< DEQUE_HPP
---
+++ src/ds/deque.tpp
@@ -0,0 +1,9 @@
+#ifndef DEQUE_TPP
+#define DEQUE_TPP
+
+namespace ds
+{
+ /// @todo
+}
+
+#endif //!< DEQUE_TPP
