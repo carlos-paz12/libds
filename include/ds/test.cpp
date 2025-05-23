662cfcc31c9f8e345c5b5479181e98ba851e972d
Leandro Andrade <leandro.andrade.401@ufrn.edu.br>
2025-05-23 16:59:44 -0300

    update  iteration test class without cmake

--- include/ds/test.cpp
+++ include/ds/test.cpp
@@ -1,11 +1,12 @@
-#include "deque.hpp"  
+#include "deque.hpp"
 #include <iostream>
+
 int main() {
     ds::Deque<int> d(5, 42); // Cria deque com 5 elementos iguais a 42
 
     auto it = d.begin();     // Iterador para o primeiro elemento
 
-    std::cout << "Testando pré-incremento (++it):\n";
+    std::cout << "Testando begin() e pré-incremento (++it):\n";
     for (int i = 0; i < 5; ++i) {
         std::cout << *it << ' '; // Deve imprimir: 42 42 42 42 42
         ++it;
@@ -18,6 +19,28 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         std::cout << *it << ' '; // Deve imprimir: 42 42 42 42 42
         it++;
+    }
+    std::cout << "\n";
+
+    // Reposiciona o iterador para o final
+    it = d.begin();
+    for (int i = 0; i < 5; ++i) ++it; // avança até o final (equivalente a end())
+
+    std::cout << "Testando pré-decremento (--it):\n";
+    for (int i = 0; i < 5; ++i) {
+        --it;
+        std::cout << *it << ' '; // Deve imprimir: 42 42 42 42 42
+    }
+    std::cout << "\n";
+
+    // Reposiciona o iterador para o final novamente
+    it = d.begin();
+    for (int i = 0; i < 5; ++i) ++it;
+
+    std::cout << "Testando pós-decremento (it--):\n";
+    for (int i = 0; i < 5; ++i) {
+        it--;
+        std::cout << *it << ' '; // Deve imprimir: 42 42 42 42 42
     }
     std::cout << "\n";
 
