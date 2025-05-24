950a7a89289c6cc14084692e46c75fe032fe671e
Leandro <leandro.andrade.401@ufrn.edu.br>
2025-05-24 17:51:06 -0300

    test of  'friend Iterator operator+' and documentation of class

--- include/ds/test.cpp
+++ include/ds/test.cpp
@@ -1,21 +1,33 @@
+/**
+ * @file test.cpp
+ * @author José Carlos da Paz Silva (carlos.paz.707@ufrn.edu.br)
+ * @author Leandro Andrade (leandro.andrade.401@ufrn.edu.br)
+ * @brief  class de testes do Deque sem cmake
+ * @version 0.1
+ * @date 2025-05-24
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include "deque.hpp"
 #include <iostream>
 
 int main() {
   std::cout << "testando o construtor padrão\n";
   ds::Deque<int> de; // Cria deque vazio
-  std::cout << de.to_string_full ();
+  std::cout << de.to_string_full (); // delhalhe do deque
   std::cout << "\n";
   std::cout << "testando o construtor com valor padrão\n";
-  ds::Deque<int> d(5, 42); // Cria deque com 5 elementos iguais a 42
-  std::cout << d.to_string_full();
+  ds::Deque<int> d(5, 42); // Cria deque com 5 elementos iguais a 42 * i, d  = {0, 42, 84, 126, 168 }
+  std::cout << d.to_string_full(); // delhalhe do deque
   std::cout << "\n";
 
     auto it = d.begin();     // Iterador para o primeiro elemento
 
     std::cout << "Testando begin() e pré-incremento (++it):\n";
+   //deve imprimir : 0 , 42, 84, 126, 168
     for (int i = 0; i < 5; ++i) {
-        std::cout << it.to_string() << "-> value: " << *it <<"\n"; // Deve imprimir: 42 42 42 42 42
+        std::cout << it.to_string() << "-> value: " << *it <<"\n"; 
         ++it;
     }
     std::cout << "\n";
@@ -23,8 +35,9 @@ int main() {
     it = d.begin(); // Reseta o iterador
 
     std::cout << "Testando pós-incremento (it++):\n";
+    // deve imprimir : 0 , 42, 84, 126, 168
     for (int i = 0; i < 5; ++i) {
-        std::cout << it.to_string() << "-> value: " << *it <<"\n"; // Deve imprimir: 42 42 42 42 42
+        std::cout << it.to_string() << "-> value: " << *it <<"\n"; 
         it++;
     }
     std::cout << "\n";
@@ -34,29 +47,36 @@ int main() {
     for (int i = 0; i < 5; ++i) ++it; // avança até o final (equivalente a end())
 
     std::cout << "Testando pré-decremento (--it):\n";
+    // deve imprimir : 168, 126, 84, 42, 0
     for (int i = 0; i < 5; ++i) {
         --it;
-        std::cout << it.to_string() << "-> value: " << *it <<"\n";// Deve imprimir: 42 42 42 42 42
+        std::cout << it.to_string() << "-> value: " << *it <<"\n";
     }
     std::cout << "\n";
 
     // Reposiciona o iterador para o final novamente
     it = d.begin();
-    for (int i = 0; i < 5; ++i) ++it;
-
+    for (int i = 0; i < 5; ++i) ++it; // avança até o final (equivalente a end())
+    //deve imprimir : 0 , 42, 84, 126, 168
     std::cout << "Testando pós-decremento (it--):\n";
     for (int i = 0; i < 5; ++i) {
         it--;
-        std::cout << it.to_string() << "-> value: " << *it <<"\n";// Deve imprimir: 42 42 42 42 42
+        std::cout << it.to_string() << "-> value: " << *it <<"\n";
     }
     std::cout << "\n";
 
     std::cout << "Testando o it + numero:\n";
     it = d.begin();
     auto it2 = it + 3; // Avança 3 posições
-    std::cout << it.to_string() << "-> value: " << *it <<"\n"; // Deve imprimir: 42
-    std::cout << it2.to_string() << "-> value: " << *it2 <<"\n"; // Deve imprimir: 42
+    std::cout << it.to_string() << "-> value: " << *it <<"\n"; // Deve imprimir: 0
+    std::cout << it2.to_string() << "-> value: " << *it2 <<"\n"; // Deve imprimir: 128
     std::cout << "\n";
+
+    std::cout << "Testando o numero + it:\n";
+    it = d.begin();
+    auto it3 = 3 + it; // Avança 3 posições
+    std::cout << it.to_string() << "-> value: " << *it <<"\n"; // Deve imprimir: 0
+    std::cout << it3.to_string() << "-> value: " << *it3 <<"\n"; // Deve imprimir: 128
 
     return 0;
 }
