0be2073d556dc1457fe3bc812db7adabe9bf15f8
Leandro <leandro.andrade.401@ufrn.edu.br>
2025-05-24 18:51:00 -0300

    Implementation  iterator '  operator!='

--- include/ds/deque.hpp
+++ include/ds/deque.hpp
@@ -148,7 +148,9 @@ Iterator &operator++() {
   bool operator==(const Iterator &other) const { 
     return (m_current == other.m_current) and (m_block == other.m_block);
   }
-  bool operator!=(const Iterator &other) const { return false; }
+  bool operator!=(const Iterator &other) const { 
+    return not(*this == other);
+   }
   bool operator<(const Iterator &other) const { return false; }
   bool operator>(const Iterator &other) const { return false; }
   bool operator<=(const Iterator &other) const { return false; }
