4b499a974cdf2d1d28f61ce45231543bafb5442c
carlos-paz12 <capz.devel@gmail.com>
2025-05-27 08:38:25 -0300

    add first doxygen comments in `deque.hpp`

--- include/ds/deque.hpp
+++ include/ds/deque.hpp
@@ -24,7 +24,6 @@ template<typename T, std::size_t BlockSize, typename BlockItr, typename ItemItr>
 class Iterator
 {
 public:
-  //== Typical iterator aliases
   using value_type = T;
   using reference = value_type&;
   using const_reference = const value_type&;
@@ -33,45 +32,23 @@ public:
   using iterator_category = std::random_access_iterator_tag;
 
 private:
-  BlockItr m_block;  //!< The block the iterator points to.
-  ItemItr m_current; //!< The last location where an insertion happened inside
-                     //!< the block.
+  BlockItr m_block;  //!< Ponteiro para o bloco atual.
+  ItemItr m_current; //!< Ponteiro para o elemento dentro do bloco atual.
 
-  // We need to grant this friendship to allow Deque access to the iterator's
-  // private attributes.
+  // [!] Nós Precisamos de conceder esta amizade para permitir que o Deque acesso os atributos privados do iterador.
   friend class Deque<T>;
 
 public:
-  /// Default constructor.
-  Iterator()
-    : m_block()
-    , m_current()
-  {
-  }
+  /// @brief Construtor padrão.
+  Iterator() : m_block(), m_current() {}
 
-  /// Copy constructor (permite conversão de não-const para const)
-  template<typename OtherBlockItr, typename OtherItemItr>
-  Iterator(const Iterator<T, BlockSize, OtherBlockItr, OtherItemItr>& other)
-    : m_block(other.m_block)
-    , m_current(other.m_current)
-  {
-  }
+  /// @brief Construtor utilitário.
+  Iterator(const BlockItr& block, const ItemItr& current) : m_block(block), m_current(current) {}
 
-  /// Useful constructor.
-  Iterator(BlockItr block, ItemItr current)
-    : m_block(block)
-    , m_current(current)
-  {
-  }
+  /// @brief Construtor de cópia.
+  Iterator(const Iterator& other) : m_block(other.m_block), m_current(other.m_current) {}
 
-  /// Copy constructor.
-  Iterator(const Iterator& other)
-    : m_block(other.m_block)
-    , m_current(other.m_current)
-  {
-  }
-
-  /// Assigment operator.
+  /// @brief Operador de atribuição.
   Iterator& operator=(const Iterator& other)
   {
     if (this != &other)
@@ -82,30 +59,31 @@ public:
     return *this;
   }
 
-  /// Dereference operator.
+  /// @brief Operador de dereferência.
   reference operator*() { return *m_current; }
 
-  // Pre-increment operator.
+  /// @brief Operador de pré-incremento.
   Iterator& operator++()
   {
-    m_current++; // Avança o iterador dentro do bloco atual
-    if (m_current == (*m_block)->end())
-    {                                  // Se chegou ao fim do bloco
-      m_block++;                       // Avança para o próximo bloco
-      m_current = (*m_block)->begin(); // Posiciona no início do novo bloco
+    m_current++;                        // [!] Avança o iterador dentro do bloco atual
+    if (m_current == (*m_block)->end()) // [!] Verifica se chegou no fim do bloco atual.
+    {
+      // [!] Se chegou...
+      m_block++;                       // [!] avança para o próximo bloco e...
+      m_current = (*m_block)->begin(); // [!] posiciona o ponteiro do elemento para o início do novo bloco.
     }
     return *this;
   }
 
-  /// Post-increment operator.
+  /// @brief Operador de pós-incremento.
   Iterator operator++(int)
   {
-    Iterator temp = *this; // salva estado atual
-    ++(*this);             // chama o pré-incremento
-    return temp;           // retorna o iterador antigo
+    Iterator temp = *this; // [!] Armazena temporariamente o estado atual do iterador.
+    ++(*this);             // [!] Chama o pré-incremento.
+    return temp;           // [!] Retorna o iterador antigo.
   }
 
-  /// Pre-decremente operator.
+  /// @brief Operador de pré-incremento.
   Iterator& operator--()
   {
     if (m_current == (*m_block)->begin())
@@ -117,14 +95,15 @@ public:
     return *this;
   }
 
-  /// Post-decremente operator.
+  /// @brief Operador de pós-incremento.
   Iterator operator--(int)
   {
     Iterator temp = *this; // salva estado atual
     --(*this);             // chama o Pre-decremente
-    return temp;           // retorna o iterador antigo
+    return temp;           // @brief Operador de pré-incrementoretorna o iterador antigo
   }
-  // it = it2 + 3; or it = it2 + (-3)
+
+  /// @brief Operador de adição à direita.
   Iterator operator+(difference_type n) const
   {
     Iterator temp = *this; // salva estado atual
@@ -134,7 +113,8 @@ public:
     }
     return temp; // Retorna o iterador avançado
   }
-  // it = 3 + it2; or it = (-3) + it2
+
+  /// @brief Operador de adição à esquerda.
   friend Iterator operator+(Iterator::difference_type n, const Iterator& other)
   {
     Iterator temp = other; // salva estado atual
@@ -144,7 +124,8 @@ public:
     }
     return temp; // Retorna o iterador avançado
   }
-  // it = it2 - 3; or it = it2 - (-3)
+
+  /// @brief Operador de subtração à direita.
   Iterator operator-(difference_type n) const
   {
     Iterator temp = *this; // salva estado atual
@@ -154,20 +135,22 @@ public:
     }
     return temp; // Retorna o iterador retrocedido
   }
-  // it += 3; or it += -3;
+
+  /// @brief Operador de salto para frente.
   Iterator operator+=(difference_type n)
   {
     *this = *this + n; // Chama o operador de soma
     return *this;      // Retorna o iterador atualizado
   }
-  // it -= 3; or it -= -3;
+
+  /// @brief Operador de salto para trás.
   Iterator operator-=(difference_type n)
   {
     *this = *this - n; // Chama o operador de subtração
     return *this;      // Retorna o iterador atualizado
   }
-  // it = it2 - it3;
-  // The *this iterator is usually the farther iterator (down the Deque).
+
+  /// @brief Operadador de subtração entre iteradores.
   difference_type operator-(const Iterator& other) const
   {
     // Diferença de blocos
@@ -181,19 +164,66 @@ public:
     return block_diff + (this_offset - other_offset);
   }
 
+  /**
+   * @brief Compara se o iterador à esquerda é igual ao iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se os dois iteradores são iguais, `false` caso contrário.
+   */
   bool operator==(const Iterator& other) const { return (m_current == other.m_current) and (m_block == other.m_block); }
+
+  /**
+   * @brief Compara se o iterador à esquerda é diferente do iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se os dois iteradores são diferentes, `false` caso contrário.
+   */
   bool operator!=(const Iterator& other) const { return not(*this == other); }
+
+  /**
+   * @brief Compara se o iterador à esquerda é menor que o iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se o iterador à esquerda é menor que o iterador à direita, `false` caso contrário.
+   */
   bool operator<(const Iterator& other) const { return (m_block < other.m_block) or ((m_block == other.m_block) and (m_current < other.m_current)); }
+
+  /**
+   * @brief Compara se o iterador à esquerda é maior que o iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se o iterador à esquerda é maior que o iterador à direita, `false` caso contrário.
+   */
   bool operator>(const Iterator& other) const { return not(*this < other); }
+
+  /**
+   * @brief Compara se o iterador à esquerda é menor ou igual que o iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se o iterador à esquerda é menor ou igual que o iterador à direita, `false` caso contrário.
+   */
   bool operator<=(const Iterator& other) const { return (*this < other) or (*this == other); }
+
+  /**
+   * @brief Compara se o iterador à esquerda é maior ou igual que o iterador à direita.
+   *
+   * @param other Iterador à direita.
+   *
+   * @return true Se o iterador à esquerda é maior ou igual que o iterador à direita, `false` caso contrário.
+   */
   bool operator>=(const Iterator& other) const { return not(*this < other); }
 
-  pointer get_current() const { return m_current; } // para o to_string_full
-  BlockItr get_block() const { return m_block; }    // para o to_string_full
-
+  /**
+   * @brief Retorna o conteúdo do iterador como uma string.
+   */
   [[nodiscard]] std::string to_string() const
   {
-    std::ostringstream oss;
+    std::ostringstream oss{};
     oss << "[&block: " << *m_block << ", &current: " << m_current << "]";
     return oss.str();
   }
