33ed0ae59661edc5d3f12b132f37315dc1bfe52d
carlos-paz12 <capz.devel@gmail.com>
2025-06-01 17:46:27 -0300

    define the interface for Deque member function tests

--- tests/ds/deque/driver.cpp
+++ tests/ds/deque/driver.cpp
@@ -10,11 +10,11 @@
 #define NO 0
 
 int main() {
-  constexpr std::array<int, 5> values_i{ 1, 2, 3, 4, 5 };
+  // constexpr std::array<int, 5> values_i{ 1, 2, 3, 4, 5 };
   constexpr std::array<int, 5> source_i{ 6, 7, 8, 9, 0 };
 
   std::cout << ">>> Testing out deque with integers.\n";
-  run_regular_deque_tests<int, 5>(values_i, source_i);
+  run_regular_deque_tests<int, 5>(source_i);
 
   // std::array<std::string, 5> values_s{"1", "2", "3", "4", "5"};
   // std::array<std::string, 5> source_s{"6", "7", "8", "9", "10"};
--- tests/ds/deque/methods_tests.hpp
+++ tests/ds/deque/methods_tests.hpp
@@ -1,124 +1,223 @@
 #ifndef DEQUE_METHODS_TESTS_HPP
 #define DEQUE_METHODS_TESTS_HPP
 
-#include <array>
-#include <cstddef>
-#include <deque>
+#include <array>   // std::array
+#include <cstddef> // std::size_t
 
-#include "ds/deque.hpp"
-#include "test_manager.hpp"
+#include "ds/deque.hpp"     // ds::Deque
+#include "test_manager.hpp" // TestManager
 
+// ==========================================
+// [*] Test Configuration Flags
+// Use YES or NO to enable/disable each test.
+// ==========================================
 #define YES 1
 #define NO 0
 
-#define DEFAULT_CTRO YES
-#define FILL_CTRO YES
-#define RANGE_CTRO YES
-#define COPY_CTRO YES
-#define INIT_LIST_CTRO YES
-#define ASSIGNMENT_COPY_OP YES
+#define DEFAULT_CTRO YES     // [!] Test the construction of an empty Deque.
+#define FILL_CTRO YES        // [!] Test the construction of a Deque with `n` elements.
+#define RANGE_CTRO YES       // [!] Test the construction of a Deque from a given interval.
+#define COPY_CTRO YES        // [!] Test the construction of a Deque from another Deque.
+#define INIT_LIST_CTRO YES   // [!] Test the construction of a Deque from an initializer list.
+#define ASSIGN_OP YES        // [!] Test the assignment of a Deque.
+#define SIZE NO              // [!] Test the size of a Deque.
+#define RESIZE NO            // [!] Test the resizing of a Deque.
+#define CAPACITY NO          // [!] Test the capacity of a Deque.
+#define EMPTY NO             // [!] Test the empty state of a Deque.
+#define SHRINK_TO_FIT NO     // [!] Test the reduction of a Deque's capacity.
+#define FRONT NO             // [!] Test the access to the first element of a Deque.
+#define FRONT_CONST NO       // [!] Test the access to the first element of a const Deque.
+#define BACK NO              // [!] Test the access to the last element of a Deque.
+#define BACK_CONST NO        // [!] Test the access to the last element of a const Deque.
+#define ASSIGN NO            // [!] Test the assignment of a Deque using the assign() method.
+#define ASSIGN_INIT_LIST NO  // [!] Test the assignment of a Deque from an initializer list using assign().
+#define ASSIGN_RANGE NO      // [!] Test the assignment of a Deque from a range using assign().
+#define PUSH_FRONT NO        // [!] Test inserting an element at the front of a Deque.
+#define PUSH_BACK NO         // [!] Test inserting an element at the back of a Deque.
+#define POP_FRONT NO         // [!] Test removing the first element of a Deque.
+#define POP_BACK NO          // [!] Test removing the last element of a Deque.
+#define INSERT NO            // [!] Test inserting an element at a specific position in a Deque.
+#define INSERT_FILL NO       // [!] Test inserting multiple copies of an element at a position in a Deque.
+#define INSERT_INIT_LIST NO  // [!] Test inserting elements from an initializer list into a Deque.
+#define INSERT_RANGE NO      // [!] Test inserting a range of elements into a Deque.
+#define ERASE NO             // [!] Test erasing an element at a specific position in a Deque.
+#define ERASE_CONST NO       // [!] Test erasing an element at a specific position in a const Deque.
+#define ERASE_RANGE NO       // [!] Test erasing a range of elements from a Deque.
+#define ERASE_RANGE_CONST NO // [!] Test erasing a range of elements from a const Deque.
+#define CLEAR NO             // [!] Test clearing all elements from a Deque.
 
 template<typename T, std::size_t size>
-void run_regular_deque_tests(const std::array<T, size>& values, const std::array<T, size>& source) {
-  TestManager tm{ "Methods testing" };
+void run_regular_deque_tests(const std::array<T, size>& src) {
+  TestManager tmanager{ "Methods testing" };
 
 #if DEFAULT_CTRO
   {
-    BEGIN_TEST(tm, "Default constructor", "ds::Deque<T> dq;");
+    BEGIN_TEST(tmanager, "Default constructor", "ds::Deque<T> deque;");
 
-    ds::Deque<T> dq;
+    ds::Deque<T> deque;
 
-    EXPECT_EQ(dq.size(), 0);
-    EXPECT_TRUE(dq.empty());
+    EXPECT_EQ(deque.size(), 0);
+    EXPECT_TRUE(deque.empty());
   }
 #endif
 
 #if FILL_CTRO
   {
-    BEGIN_TEST(tm, "Fill constructor", "ds::Deque<T> dq(S, val);");
+    BEGIN_TEST(tmanager, "Fill constructor", "ds::Deque<T> deque(size, val);");
 
-    ds::Deque<T> dq(size, 42);
+    ds::Deque<T> deque(size, 42);
 
-    EXPECT_EQ(dq.size(), size);
-    EXPECT_FALSE(dq.empty());
+    EXPECT_EQ(deque.size(), size);
+    EXPECT_FALSE(deque.empty());
 
-    for (std::size_t i{ 0 }; i < size; ++i) {
-      EXPECT_EQ(dq[i], 42);
-    }
+    for (std::size_t i{ 0 }; i < size; ++i) EXPECT_EQ(deque[i], 42);
   }
 #endif
 
 #if RANGE_CTRO
   {
-    BEGIN_TEST(tm, "Range constructor", "ds::Deque<T> dq(source.begin(), source.end());");
+    BEGIN_TEST(tmanager, "Range constructor", "ds::Deque<T> deque(src.begin(), src.end());");
 
-    ds::Deque<T> dq(source.begin(), source.end());
+    ds::Deque<T> deque(src.begin(), src.end());
 
-    EXPECT_EQ(dq.size(), source.size());
-    EXPECT_FALSE(dq.empty());
+    EXPECT_EQ(deque.size(), src.size());
+    EXPECT_FALSE(deque.empty());
 
-    for (std::size_t i{ 0 }; i < size; ++i) {
-      EXPECT_EQ(dq[i], source[i]);
-    }
+    for (std::size_t i{ 0 }; i < size; ++i) EXPECT_EQ(deque[i], src[i]);
+
+    deque[0] = 100;
+    EXPECT_NE(deque[0], src[0]);
   }
 #endif
 
 #if COPY_CTRO
   {
-    BEGIN_TEST(tm, "Copy constructor", "ds::Deque<T> dq2(dq1);");
+    BEGIN_TEST(tmanager, "Copy constructor", "ds::Deque<T> deque2(deque1);");
 
-    ds::Deque<T> dq1(source.begin(), source.end());
-    ds::Deque<T> dq2(dq1);
+    ds::Deque<T> deque1(src.begin(), src.end());
+    ds::Deque<T> deque2(deque1);
 
-    EXPECT_EQ(dq2.size(), dq1.size());
-    EXPECT_EQ(dq2.capacity(), dq1.capacity());
-    EXPECT_FALSE(dq2.empty());
+    EXPECT_EQ(deque2.size(), deque1.size());
+    EXPECT_EQ(deque2.capacity(), deque1.capacity());
+    EXPECT_FALSE(deque2.empty());
 
-    for (std::size_t i{ 0 }; i < dq1.size(); ++i) {
-      EXPECT_EQ(dq2[i], dq1[i]);
-    }
+    for (std::size_t i{ 0 }; i < deque1.size(); ++i) EXPECT_EQ(deque2[i], deque1[i]);
 
-    dq1[0] = 99;
-
-    EXPECT_NE(dq2[0], dq1[0]);
+    deque1[0] = 99;
+    EXPECT_NE(deque2[0], deque1[0]);
   }
 #endif
 
 #if INIT_LIST_CTRO
   {
-    BEGIN_TEST(tm, "Initializer list", "ds::Deque<T> dq{source[0], source[1], source[2], source[3], source[4]};");
+    BEGIN_TEST(tmanager, "Initializer list", "ds::Deque<T> deque({src[0], src[1], src[2], src[3], src[4]});");
 
-    ds::Deque<T> dq{ source[0], source[1], source[2], source[3], source[4] };
+    ds::Deque<T> deque({ src[0], src[1], src[2], src[3], src[4] });
 
-    EXPECT_EQ(dq.size(), 5);
-    EXPECT_FALSE(dq.empty());
+    EXPECT_EQ(deque.size(), 5);
+    EXPECT_FALSE(deque.empty());
 
-    for (std::size_t i{ 0 }; i < size; ++i) {
-      EXPECT_EQ(dq[i], source[i]);
-    }
+    for (std::size_t i{ 0 }; i < size; ++i) EXPECT_EQ(deque[i], src[i]);
+
+    deque[0] = 100;
+    EXPECT_NE(deque[0], src[0]);
   }
 #endif
 
-#if ASSIGNMENT_COPY_OP
+#if ASSIGN_OP
   {
-    BEGIN_TEST(tm, "Copy assignment", "dq2 = dq1;");
+    BEGIN_TEST(tmanager, "Copy assignment", "deque2 = deque1;");
 
-    ds::Deque<T> dq1{ source[0], source[1], source[2], source[3], source[4] };
-    ds::Deque<T> dq2;
+    ds::Deque<T> deque1({ src[0], src[1], src[2], src[3], src[4] });
+    ds::Deque<T> deque2;
 
-    dq2 = dq1;
+    deque2 = deque1;
 
-    EXPECT_EQ(dq2.size(), dq1.size());
-    EXPECT_EQ(dq2.capacity(), dq1.capacity());
-    EXPECT_FALSE(dq2.empty());
+    EXPECT_EQ(deque2.size(), deque1.size());
+    EXPECT_EQ(deque2.capacity(), deque1.capacity());
+    EXPECT_FALSE(deque2.empty());
 
-    dq2[1] = 100;
-
-    EXPECT_NE(dq2[1], dq1[1]);
+    deque2[1] = 100;
+    EXPECT_NE(deque2[1], deque1[1]);
   }
 #endif
 
-  tm.summary();
+#if SIZE
+#endif
+
+#if RESIZE
+#endif
+
+#if CAPACITY
+#endif
+
+#if EMPTY
+#endif
+
+#if SHRINK_TO_FIT
+#endif
+
+#if FRONT
+#endif
+
+#if FRONT_CONST
+#endif
+
+#if BACK
+#endif
+
+#if BACK_CONST
+#endif
+
+#if ASSIGN
+#endif
+
+#if ASSIGN_INIT_LIST
+#endif
+
+#if ASSIGN_RANGE
+#endif
+
+#if PUSH_FRONT
+#endif
+
+#if PUSH_BACK
+#endif
+
+#if POP_FRONT
+#endif
+
+#if POP_BACK
+#endif
+
+#if INSERT
+#endif
+
+#if INSERT_FILL
+#endif
+
+#if INSERT_INIT_LIST
+#endif
+
+#if INSERT_RANGE
+#endif
+
+#if ERASE
+#endif
+
+#if ERASE_CONST
+#endif
+
+#if ERASE_RANGE
+#endif
+
+#if ERASE_RANGE_CONST
+#endif
+
+#if CLEAR
+#endif
+
+  tmanager.summary();
 }
 
-#endif //!< DEQUE_METHODS_TESTS_HPP
+#endif // DEQUE_METHODS_TESTS_HPP
